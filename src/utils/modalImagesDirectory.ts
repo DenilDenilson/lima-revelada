9b1c3dbad67bf66fb4f2385ddb2ef384aacf8dd8
Denilson <jordanpreseleccion@gmail.com>
2025-07-14 02:11:14 -0500

    Add modal components for displaying sources, history, and images; refactor modal imports and update image directory

--- src/utils/modalImagesDirectory.ts
+++ src/utils/modalImagesDirectory.ts
@@ -1,6 +1,7 @@
 import Matusita from '../assets/OptimizedImages/paranormal-01-matusita.webp';
 
 export const modalImagesDirectory = {
+	'default-id': Matusita,
 	'paranormal-01': Matusita,
 	'paranormal-02': Matusita,
 };
