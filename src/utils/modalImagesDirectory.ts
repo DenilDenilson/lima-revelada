068cba2736013a7129657f073c98fe37d7462b6d
Denilson <jordanpreseleccion@gmail.com>
2025-07-18 03:46:54 -0500

    Add image for Piedra Horadada and update modal image directory

--- src/utils/modalImagesDirectory.ts
+++ src/utils/modalImagesDirectory.ts
@@ -1,7 +1,8 @@
 import Matusita from '../assets/OptimizedImages/paranormal-01-matusita.webp';
+import PiedraHoradada from '../assets/OptimizedImages/Paranormal-02-PiedraHoradada.webp';
 
 export const modalImagesDirectory = {
 	'default-id': Matusita,
 	'paranormal-01': Matusita,
-	'paranormal-02': Matusita,
+	'paranormal-02': PiedraHoradada,
 };
