fddfde24ee12f99fae66b832472da4d16e8d37c3
Denilson <jordanpreseleccion@gmail.com>
2025-07-19 03:53:56 -0500

    Add images for Hotel El Comercio and Puerta Dimensional Lince to modal images directory

--- src/utils/modalImagesDirectory.ts
+++ src/utils/modalImagesDirectory.ts
@@ -1,8 +1,12 @@
 import Matusita from '../assets/OptimizedImages/paranormal-01-matusita.webp';
 import PiedraHoradada from '../assets/OptimizedImages/Paranormal-02-PiedraHoradada.webp';
+import HotelComercio from '../assets/OptimizedImages/Paranormal-03-HotelElComercio.webp';
+import PortalDimensionalLince from '../assets/OptimizedImages/Paranormal-04-PuertaDimensionalLince.webp';
 
 export const modalImagesDirectory = {
 	'default-id': Matusita,
 	'paranormal-01': Matusita,
 	'paranormal-02': PiedraHoradada,
+	'paranormal-03': HotelComercio,
+	'paranormal-04': PortalDimensionalLince,
 };
