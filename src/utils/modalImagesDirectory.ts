f2c403eb88c07d0a531904973c359f969ac10651
Denilson <jordanpreseleccion@gmail.com>
2025-07-24 00:16:41 -0500

    Add new images and JSON data for curiosidades ocultas, espacios singulares, and instagrameables

--- src/utils/modalImagesDirectory.ts
+++ src/utils/modalImagesDirectory.ts
@@ -1,8 +1,21 @@
-import Matusita from '../assets/OptimizedImages/paranormal-01-matusita.webp';
-import PiedraHoradada from '../assets/OptimizedImages/Paranormal-02-PiedraHoradada.webp';
-import HotelComercio from '../assets/OptimizedImages/Paranormal-03-HotelElComercio.webp';
-import PortalDimensionalLince from '../assets/OptimizedImages/Paranormal-04-PuertaDimensionalLince.webp';
-import CasaEncarnacion from '../assets/OptimizedImages/Paranormal-05-CasaEncarnacion.webp';
+// PARANORMAL MODAL IMAGES DIRECTORY
+import Matusita from '../assets/OptimizedImages/paranormal/Paranormal-01-matusita.webp';
+import PiedraHoradada from '../assets/OptimizedImages/paranormal/Paranormal-02-PiedraHoradada.webp';
+import HotelComercio from '../assets/OptimizedImages/paranormal/Paranormal-03-HotelElComercio.webp';
+import PortalDimensionalLince from '../assets/OptimizedImages/paranormal/Paranormal-04-PuertaDimensionalLince.webp';
+import CasaEncarnacion from '../assets/OptimizedImages/paranormal/Paranormal-05-CasaEncarnacion.webp';
+
+// INSTAGREMEABLE MODAL IMAGES DIRECTORY
+import ArcoChino from '../assets/OptimizedImages/instagrameables/ig-01-ArcoChino.webp';
+import LeonesFu from '../assets/OptimizedImages/instagrameables/ig-02-LeonesFu.webp';
+
+// CURIOSIDADES OCULTAS MODAL IMAGES DIRECTORY
+import CupulaAcustica from '../assets/OptimizedImages/curiosidadesOcultas/oculto-01-CupulaAcustica.webp';
+import PrimeraEscaleraElectrica from '../assets/OptimizedImages/curiosidadesOcultas/oculto-02-PrimeraEscaleraElectrica.webp';
+
+// ESPACIOS SINGULARES MODAL IMAGES DIRECTORY
+import BalconMasLargo from '../assets/OptimizedImages/espaciosSingulares/singular-01-BalconMasLargo.webp';
+import BarLaCatedral from '../assets/OptimizedImages/espaciosSingulares/singular-02-BarLaCatedral.webp';
 
 export const modalImagesDirectory = {
 	'default-id': Matusita,
@@ -11,4 +24,10 @@ export const modalImagesDirectory = {
 	'paranormal-03': HotelComercio,
 	'paranormal-04': PortalDimensionalLince,
 	'paranormal-05': CasaEncarnacion,
+	'instagrameable-01': ArcoChino,
+	'instagrameable-02': LeonesFu,
+	'curiosidadOculta-01': CupulaAcustica,
+	'curiosidadOculta-02': PrimeraEscaleraElectrica,
+	'espacioSingular-01': BalconMasLargo,
+	'espacioSingular-02': BarLaCatedral,
 };
